cdb857b35f95e70b21a9b3ae901d96838e68fd11
alexms13 <alexartsms@gmail.com>
2025-05-10 13:40:10 +0400

    Sixth task completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(value) {
+    const number = Number.parseFloat(value);
+    if (isNaN(number)) {
+        throw new Error('Невалидное значение');
+    }
+    return number;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);      
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if ((a + b) < c || (b + c) < a ||(a + c) < b) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = (this.a + this.b + this.c) / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return parseFloat(area.toFixed(3)); 
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
